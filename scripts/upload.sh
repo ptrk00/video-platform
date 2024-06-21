a2cb38d026c698dae534874cbb8ebaecf2e9bcb1
Piotr Pyka <pyka.piotr.00@gmail.com>
2024-06-21 20:40:28 +0000

    Add minio dashboards

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,3 +1,3 @@
 
-curl -X POST http://localhost:8080/upload -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwiaWQiOjEsImV4cCI6MTcxOTAwMTE0OH0.1tJhybuNwjVEmjfptMWusHp7vtSZU7ZR1v0FafZH4_A' -F "myfile=@login.sh"
+curl -X POST http://localhost:8080/upload -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwiaWQiOjEsImV4cCI6MTcxOTAwNTQ3MH0.uXzUZBjpZv5HrfKXTmXwCB7NbJE77iaFX_NkUl1rZQw' -F "myfile=@login.sh"
 # curl -X POST http://localhost:8080/upload -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIyIiwiZXhwIjoxNzE4OTg5MDk3fQ.kuWTYMhIANp14j79pdCaqJowb8U3Zr4xlrCZeT7Xukg' -F "myfile=@login.sh"
---
+++ scripts/upload_huge.sh
@@ -0,0 +1,3 @@
+
+curl -X POST http://localhost:8080/upload -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwiaWQiOjEsImV4cCI6MTcxOTAwNTQ3MH0.uXzUZBjpZv5HrfKXTmXwCB7NbJE77iaFX_NkUl1rZQw' -F "myfile=@/home/ppyka/hugefile.dat"
+# curl -X POST http://localhost:8080/upload -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIyIiwiZXhwIjoxNzE4OTg5MDk3fQ.kuWTYMhIANp14j79pdCaqJowb8U3Zr4xlrCZeT7Xukg' -F "myfile=@login.sh"
