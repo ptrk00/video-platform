de0a2fe349f8d567c1904abb60c3fcc1115e72e4
Piotr Pyka <pyka.piotr.00@gmail.com>
2024-06-21 11:31:29 +0000

    PoC Opa integration

---
+++ scripts/upload.sh
@@ -0,0 +1,3 @@
+
+curl -X POST http://localhost:8080/upload -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwiZXhwIjoxNzE4OTcxODI2fQ.zBSaSuZnOVNooPj4doD6VzzwE1n1RnzzpAPksT0ga5U ' -F "myfile=@login.sh"
+# curl -X POST http://localhost:8080/upload -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIyIiwiZXhwIjoxNzE4OTcyOTgzfQ.gtnG2pgyLVTUeo2vFojLffjX9SjaDj5R5VuNCMjQQpE ' -F "myfile=@login.sh"
