c1c37c10c5adeccc83b304e357ace7101aef7c30
Piotr Pyka <pyka.piotr.00@gmail.com>
2024-06-21 06:27:49 +0000

    PoC otel integartion

--- go.mod
+++ go.mod
@@ -3,16 +3,23 @@ module video-platform
 go 1.22.2
 
 require (
-	github.com/minio/minio-go/v7 v7.0.70
-	github.com/nats-io/nats.go v1.31.0
-	github.com/spf13/viper v1.18.2
+	github.com/jackc/pgx/v4 v4.18.3
+	github.com/minio/minio-go/v7 v7.0.21
+	github.com/nats-io/nats.go v1.35.0
+	github.com/spf13/viper v1.9.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.52.0
+	go.opentelemetry.io/otel v1.27.0
+	go.opentelemetry.io/otel/exporters/jaeger v1.10.0
+	go.opentelemetry.io/otel/sdk v1.27.0
 	go.uber.org/zap v1.27.0
 )
 
 require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
-	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
@@ -22,29 +29,34 @@ require (
 	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
-	github.com/jackc/pgx/v4 v4.18.3 // indirect
-	github.com/klauspost/compress v1.17.6 // indirect
+	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/minio/sha256-simd v0.1.1 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/nats-io/nkeys v0.4.6 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/nats-io/nkeys v0.4.7 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
-	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/rs/xid v1.5.0 // indirect
-	github.com/sagikazarmark/locafero v0.4.0 // indirect
-	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
-	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	go.opentelemetry.io/otel/metric v1.27.0 // indirect
+	go.opentelemetry.io/otel/trace v1.27.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- uploader/cmd/main.go
+++ uploader/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/md5"
 	"crypto/sha256"
 	"database/sql"
@@ -16,6 +17,17 @@ import (
 	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/nats-io/nats.go"
 	"github.com/spf13/viper"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
+
+	// "go.opentelemetry.io/otel/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	"go.uber.org/zap"
 )
 
@@ -28,6 +40,7 @@ const (
 	minioBucketOpt       = "MINIO_BUCKET"
 	videoFormFilenameOpt = "VIDEO_FORM_FILENAME"
 	postgresDSNOpt       = "POSTGRES_DSN"
+	jaegerEndpointOpt    = "JAEGER_ENDPOINT"
 )
 
 type ServerConfig struct {
@@ -39,6 +52,7 @@ type ServerConfig struct {
 	MinioBucket       string
 	VideoFormFilename string
 	PostgresDSN       string
+	JaegerEndpoint    string
 }
 
 func buildConfig() *ServerConfig {
@@ -51,10 +65,27 @@ func buildConfig() *ServerConfig {
 		MinioBucket:       viper.GetString(minioBucketOpt),
 		VideoFormFilename: viper.GetString(videoFormFilenameOpt),
 		PostgresDSN:       viper.GetString(postgresDSNOpt),
+		JaegerEndpoint:    viper.GetString(jaegerEndpointOpt),
 	}
 }
 
 var l *zap.SugaredLogger
+
+func initTracer(config *ServerConfig) (*sdktrace.TracerProvider, error) {
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(config.JaegerEndpoint)))
+	if err != nil {
+		return nil, err
+	}
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exp),
+		sdktrace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String("uploader"),
+		)),
+	)
+	otel.SetTracerProvider(tp)
+	return tp, nil
+}
 
 func init() {
 	logger := zap.Must(zap.NewDevelopment())
@@ -74,7 +105,11 @@ func init() {
 	}
 }
 
-func publishMessage(bucketname, filename string) {
+func publishMessage(ctx context.Context, bucketname, filename string) {
+	tracer := otel.Tracer("uploader")
+	ctx, span := tracer.Start(ctx, "publishMessage")
+	defer span.End()
+
 	// Connect to a NATS server
 	nc, err := nats.Connect("nats://admin:admin@nats:4222")
 	if err != nil {
@@ -114,15 +149,44 @@ func computeChecksum(reader io.Reader) (string, string, error) {
 	return hex.EncodeToString(hashMd5.Sum(nil)), hex.EncodeToString(hashSha256.Sum(nil)), nil
 }
 
-func storeFileMetadata(db *sql.DB, filename string, filesize int64, contentType, etag, fileURL, checksum string, userID int) error {
+func storeFileMetadata(ctx context.Context, db *sql.DB, filename string, filesize int64, contentType, etag, fileURL, checksum string, userID int) error {
+	tracer := otel.Tracer("uploader")
+	_, span := tracer.Start(ctx, "storeFileMetadata")
+	defer span.End()
+	span.SetAttributes(
+		attribute.String("filename", filename),
+		attribute.String("etag", etag),
+	)
+
 	query := `INSERT INTO files (filename, filesize, content_type, etag, file_url, checksum, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7)`
-	_, err := db.Exec(query, filename, filesize, contentType, etag, fileURL, checksum, userID)
+
+	span.AddEvent("Storing file metadata in the database", trace.WithAttributes(
+		attribute.String("etag", etag),
+		attribute.String("query", query),
+	))
+	
+	_, err := db.ExecContext(ctx, query, filename, filesize, contentType, etag, fileURL, checksum, userID)
+	if err != nil {
+		span.SetStatus(codes.Error, "Failed to execute query")
+		span.RecordError(err)
+	}
 	return err
 }
 
 func main() {
 	l.Debug("Reading configuration")
 	config := buildConfig()
+
+	// Initialize OpenTelemetry tracer
+	tp, err := initTracer(config)
+	if err != nil {
+		l.Fatal("Failed to initialize OpenTelemetry tracer", zap.Error(err))
+	}
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			l.Fatal("Failed to shutdown OpenTelemetry tracer", zap.Error(err))
+		}
+	}()
 
 	// Connect to PostgreSQL
 	db, err := sql.Open("pgx", "postgresql://postgres:5432/videos?user=postgres&password=postgres")
@@ -142,6 +206,9 @@ func main() {
 	}
 
 	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		ctx, span := otel.Tracer("uploader").Start(r.Context(), "HandleUpload")
+		defer span.End()
+
 		l.Debugw("Handling video upload")
 		if r.Method != "POST" {
 			http.Error(w, "Unsupported method", http.StatusMethodNotAllowed)
@@ -177,7 +244,7 @@ func main() {
 		file.Seek(0, io.SeekStart)
 		l.Infow("Uploading file", zap.String("bucketname", config.MinioBucket),
 			zap.String("filename", handler.Filename))
-		info, err := minioClient.PutObject(r.Context(), config.MinioBucket, handler.Filename, file, fileSize, minio.PutObjectOptions{ContentType: contentType})
+		info, err := minioClient.PutObject(ctx, config.MinioBucket, handler.Filename, file, fileSize, minio.PutObjectOptions{ContentType: contentType})
 		if err != nil {
 			l.Errorw("Could not upload file", zap.String("bucketname", config.MinioBucket),
 				zap.String("filename", handler.Filename), zap.Error(err))
@@ -190,7 +257,7 @@ func main() {
 		etag := info.ETag
 
 		// Store metadata in PostgreSQL
-		err = storeFileMetadata(db, handler.Filename, fileSize, contentType, etag, fileURL, sha256Checksum, 1) // Assuming userID is 1 for this example
+		err = storeFileMetadata(ctx, db, handler.Filename, fileSize, contentType, etag, fileURL, sha256Checksum, 1) // Assuming userID is 1 for this example
 		if err != nil {
 			l.Errorw("Could not store file metadata", zap.String("filename", handler.Filename), zap.Error(err))
 			http.Error(w, "Error storing file metadata", http.StatusInternalServerError)
@@ -200,8 +267,8 @@ func main() {
 		l.Infow("Successfully uploaded file", zap.String("bucketname", config.MinioBucket),
 			zap.String("filename", handler.Filename))
 		fmt.Fprintf(w, "Successfully uploaded %s\n", handler.Filename)
-		publishMessage(config.MinioBucket, handler.Filename)
+		publishMessage(ctx, config.MinioBucket, handler.Filename)
 	})
 
-	http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
+	http.ListenAndServe(fmt.Sprintf(":%d", config.Port), otelhttp.NewHandler(http.DefaultServeMux, "Server"))
 }
